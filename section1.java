6d07242a66e535c427bfb95c345bda2137f48c38
Bilgehan0 <126520608+Bilgehan0@users.noreply.github.com>
2023-09-20 16:02:20 +0300

    Update section1.java
    Completed the second section of the assignment

--- section1.java
+++ section1.java
@@ -4,6 +4,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class section1 {
+
+    public static void displayMenu() {
+        System.out.println("Enter your choice(enter exit to quit)\n1) Find the minimum and the maximum of the array\n2) Find the average of the array\n3) Find the sum of elements with odd- and even-numbered indexes");
+    }
+    public static void getChoice() {
+        do {
+            String choice;
+            displayMenu();
+            Scanner input = new Scanner(System.in);
+            choice = input.next();
+            if (choice.equals("1")) {
+                
+            }
+            else if (choice.equals("2")) {
+                
+            }
+            else if (choice.equals("3")) {
+                
+            }
+        } while (!choice.equals("exit"));
+
+    }
     public static void main(String[] args) {
          // Given an array size, create an int array that contains random integers in the range [0,100].
          // All the remaining functionality will operate on this array.
@@ -20,6 +42,7 @@ public class section1 {
             num = rand.nextInt(100);
             nums[i] = num;
          }
+        getChoice();
       
 
     }
